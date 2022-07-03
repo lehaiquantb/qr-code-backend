d1db1ffcb53cfb645696d1b24500ed4de462b430
lehaiquantb <lehaiquantb@gmail.com>
2022-07-03 16:39:14 +0700

    Fix script

--- database/factories/role_permission.factory.ts
+++ database/factories/role_permission.factory.ts
@@ -1,17 +1,8 @@
+import { factoryExcute } from '~database/factories';
+import { FactoryDefine } from '.';
+import { PermissionEntity } from './../../src/modules/role/entity/permission.entity';
 import { RolePermissionEntity } from './../../src/modules/role/entity/role-permission.entity';
 import { RoleEntity } from './../../src/modules/role/entity/role.entity';
-import {
-    PERMISSION_ACTION,
-    PERMISSION_RESOURCE,
-} from '~common';
-import { PermissionResourceEntity } from './../../src/modules/role/entity/permission-resource.entity';
-import { PermissionActionEntity } from './../../src/modules/role/entity/permission-action.entity';
-import { factoryExcute } from '~database/factories';
-import { PermissionEntity } from './../../src/modules/role/entity/permission.entity';
-import { faker } from '@faker-js/faker';
-import { UserStatus } from './../../src/modules/user/user.constant';
-import { UserEntity } from 'src/modules/user/entity/user.entity';
-import { FactoryDefine } from '.';
 
 const rolePermissionFactory: FactoryDefine<RolePermissionEntity> = async (
     params,
